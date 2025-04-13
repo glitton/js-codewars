5d58ec170785cc6d0d584f946393df6b655f2c6d
Generosa Litton <glcodeworks@gmail.com>
2025-04-13 00:40:50 -0500

    first duplicate revisit

--- firstDup.js
+++ firstDup.js
@@ -43,9 +43,3 @@ console.log(firstDup("ode to joy")); // "o");
 console.log(firstDup("bar")); // undefined);
 console.log(firstDup("123123")); // "1");
 console.log(firstDup("!@#$!@#$")); // "!");
-
-// let word = "tweet";
-// let letter = "t";
-// let wordSlice = word.slice(1);
-
-// console.log(wordSlice.includes(letter));
