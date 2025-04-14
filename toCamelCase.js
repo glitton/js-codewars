e080172232f2da4ba48ac7134b6fe65829491292
Generosa Litton <glcodeworks@gmail.com>
2025-04-14 12:54:58 -0500

    camel case alternative

--- toCamelCase.js
+++ toCamelCase.js
@@ -23,26 +23,46 @@ Return camelCaseString
 
 */
 
+// function toCamelCase(str) {
+//   let strArray = [];
+//   if (str === "") return "";
+
+//   if (str.includes("-")) {
+//     strArray = str.split("-");
+//   } else if (str.includes("_")) {
+//     strArray = str.split("_");
+//   }
+
+//   let camelCaseString = strArray[0];
+//   for (let idx = 1; idx < strArray.length; idx++) {
+//     if (strArray[idx][0] === strArray[idx][0].toUpperCase()) {
+//       camelCaseString += strArray[idx][0] + strArray[idx].slice(1);
+//     } else {
+//       camelCaseString +=
+//         strArray[idx][0].toUpperCase() + strArray[idx].slice(1);
+//     }
+//   }
+//   return camelCaseString;
+// }
+
 function toCamelCase(str) {
-  let strArray = [];
-  if (str === "") return "";
+  let finalString = "";
 
-  if (str.includes("-")) {
-    strArray = str.split("-");
-  } else if (str.includes("_")) {
-    strArray = str.split("_");
-  }
-
-  let camelCaseString = strArray[0];
-  for (let idx = 1; idx < strArray.length; idx++) {
-    if (strArray[idx][0] === strArray[idx][0].toUpperCase()) {
-      camelCaseString += strArray[idx][0] + strArray[idx].slice(1);
+  for (let idx = 0; idx < str.length; idx++) {
+    let currChar = str[idx];
+    let nextChar = str[idx + 1];
+    if (currChar === "_" || currChar === "-") {
+      // Set currChar as empty when you find an underscore or dash, don't add it to finalString
+      if (nextChar) {
+        // Check if nextChar exists
+        finalString += nextChar.toUpperCase(); // Convert nextChar to uppercase and add it to finalString
+        idx++; // Skip the next character since we already processed it
+      }
     } else {
-      camelCaseString +=
-        strArray[idx][0].toUpperCase() + strArray[idx].slice(1);
+      finalString += currChar; // Otherwise, just add currChar to finalString
     }
   }
-  return camelCaseString;
+  return finalString;
 }
 console.log(toCamelCase("")); //""
 console.log(toCamelCase("the_stealth_warrior")); // "theStealthWarrior"
